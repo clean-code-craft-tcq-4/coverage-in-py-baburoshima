7f24ccc57cef41b317b70a5451692f5af22e5cb8
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-07-29 09:35:38 +0530

    Update after review

--- temperature_limits.py
+++ temperature_limits.py
@@ -1,8 +1,12 @@
-coolingType = "PASSIVE_COOLING", "HI_ACTIVE_COOLING","MED_ACTIVE_COOLING"
+PASSIVE = "PASSIVE_COOLING"
+HI_ACTIVE = "HI_ACTIVE_COOLING"
+MED_ACTIVE = "MED_ACTIVE_COOLING"
 
-temperature_limits = {"PASSIVE_COOLING":(0,35),
-                    "HI_ACTIVE_COOLING":(0,45),
-                    "MED_ACTIVE_COOLING":(0,40),}
+coolingType = PASSIVE, HI_ACTIVE,MED_ACTIVE
+
+temperature_limits = {PASSIVE:(0,35),
+                    HI_ACTIVE:(0,45),
+                    MED_ACTIVE:(0,40),}
 
 def get_temperature_breachlimits(coolingTypeInput):
   if coolingTypeInput in coolingType:
